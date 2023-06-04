097a37ea98996ce7a571d0952429c4ce943aaa78
roshith2001 <roshithj@gmail.com>
2023-06-04 17:10:16 +0530

    Part1 1.2

--- Part1/part1/src/App.js
+++ Part1/part1/src/App.js
@@ -6,21 +6,26 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return(
     <>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercise}
       </p>
     </>
   )
 }
+
+const Content = (props) => {
+  return(
+    <>
+      <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+      <Part part={props.part3} exercise={props.exercises3} />
+    </>
+  )
+}
+
 
 const Total = (props) => {
   return(

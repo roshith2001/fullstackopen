d5010e7fb0e6ea1dbedc9be6ab92fd84594f3b7c
roshith2001 <roshithj@gmail.com>
2023-06-12 20:27:39 +0530

    Part2 Ex2.11

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,21 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Filter from './components/Filter'
 import Input from './components/Input'
 import NumberList from './components/NumberList'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
   const [isFiltering, setIsFiltering] = useState(false)
   const [filteredPerson,setFilteredPerson] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        console.log('Succesfull', response.data)
+      })
+  },[])
 
   const handleNameChange = (value) => {
     setNewName(value)
